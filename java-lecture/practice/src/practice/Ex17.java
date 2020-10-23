5007fc0bf41f7361ea75dd6c57bafba07b1b1415
codelifee <suminjung09@gmail.com>
2020-10-23 11:45:52 +0900

    I learned how to handle exceptions properly!

--- java-lecture/practice/src/practice/Ex17.java
+++ java-lecture/practice/src/practice/Ex17.java
@@ -1,5 +1,6 @@
 package practice;
 
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class Ex17 {
@@ -8,12 +9,31 @@ public class Ex17 {
 		
 		Scanner scan = new Scanner(System.in);
 		
+//		boolean exit = false;
+//		while(!exit) {
+//			try {
+//				//if I don't use nextInt, how can I catch an error which is not a number?
+//				System.out.println("I am gonna check your age.");
+//				System.out.println("Insert your age here :");
+//				String result = scan.nextLine();
+//				int newAge = Integer.parseInt(result);
+//				
+//				String ageCheck = (newAge >= 20 && newAge <= 29) ? "20´ë" : "20´ë ¾Æ´Ô";
+//				
+//				System.out.println(ageCheck);
+//				exit = true;
+//			} catch(Exception e) {
+//				System.out.println("Please insert the number");
+//			} 
+//		}
+		
 		boolean exit = false;
-		while(!exit) {
+		do {
 			try {
 				//if I don't use nextInt, how can I catch an error which is not a number?
 				System.out.println("I am gonna check your age.");
-				System.out.println("Insert your age here :");
+				System.out.print("Insert your age here :");
+				newAge = scan.nextInt();
 				String result = scan.nextLine();
 				int newAge = Integer.parseInt(result);
 				
@@ -21,10 +41,11 @@ public class Ex17 {
 				
 				System.out.println(ageCheck);
 				exit = true;
-			} catch(Exception e) {
+			} catch(InputMismatchException  e) {
 				System.out.println("Please insert the number");
+				//e.printStackTrace();
 			} 
-		}
+		} while(!exit);
 		
 		scan.close();				
 	}
