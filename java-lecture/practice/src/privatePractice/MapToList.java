fde97bfaaa4f895568698a6152330e11a084b01c
codelifee <suminjung09@gmail.com>
2020-10-26 10:58:42 +0900

    Storing object into List

--- java-lecture/practice/src/privatePractice/MapToList.java
+++ java-lecture/practice/src/privatePractice/MapToList.java
@@ -1,34 +1,29 @@
 package privatePractice;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
 import java.util.Scanner;
 
-public class MapToList {
+public class MapToList {	
 	
 	public static void main(String[] args) {
-		
 		Scanner scan = new Scanner(System.in);
-		Map<String, String> map = new HashMap<>();		
-		List<Object> maps = new ArrayList<Object>();
-		
-		
+		Mapping[] maps = new Mapping[7];
+				
 		for(int i=0; i < 2; i++) {
+			
 			System.out.println("id: ");
 			String id = scan.nextLine();
 			System.out.println("password: ");
 			String password = scan.nextLine();
 			
-			map.put("id", id);
-			map.put("password", password);
-			
-			maps.add(map);
-			
+			maps[i] = new Mapping();
+			maps[i].setId(id);
+			maps[i].setPassword(password);						
 		}
 		
-		System.out.println(maps);
+		for(int i=0; i<2; i++) {
+			System.out.println("id is " + maps[i].getId() + " password is " + maps[i].getPassword());
+		}
+		
 		scan.close();
 	}
 }
