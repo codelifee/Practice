c56df7470e2c144409b07399a2a775cd0f8bcaa8
codelifee <suminjung09@gmail.com>
2020-10-30 11:40:00 +0900

    Calculate the total sum of odd numbers between 1~100 using double for loops

---
+++ java-lecture/practice/src/loop/Ex06.java
@@ -0,0 +1,22 @@
+package loop;
+
+public class Ex06 {
+
+	public static void main(String[] args) {
+		int i=0;
+
+		
+		int count=0;
+		int sum=0;
+		
+		for(i=22; i<76; i++) {
+			if(i % 2 == 0) {
+				sum += i;
+				count += 1;
+			}
+		}
+		
+		System.out.println("The total number of odd numbers : " + count);
+		System.out.println("The total sum of odd numbers : " + sum);
+	}
+}
---
+++ java-lecture/practice/src/loop/Ex08.java
@@ -0,0 +1,8 @@
+package loop;
+
+public class Ex08 {
+	
+	public static void main(String[] args) {
+		
+	}
+}
