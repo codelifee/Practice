03b2f96a7e47969096704b993252ff6df2e7d34b
codelifee <suminjung09@gmail.com>
2020-11-16 11:49:26 +0900

    Practice vector, Enumeration, and Iterator

--- java-lecture/practice/src/publicpractice/collection/ColGeneric03.java
+++ java-lecture/practice/src/publicpractice/collection/ColGeneric03.java
@@ -1,5 +1,23 @@
 package publicpractice.collection;
 
+import java.util.Enumeration;
+import java.util.Vector;
+
 public class ColGeneric03 {
 
+	public static void main(String[] args) {
+		Vector v = new Vector();
+		for(int i=0; i<=3; i++) {
+			v.add(new Integer(i*10));
+		}
+		
+		System.out.println(v.elements());
+		Enumeration enu2 = v.elements();
+		
+		while(enu2.hasMoreElements()) {
+			System.out.println(" " + enu2.nextElement());
+		}
+		
+		System.out.println(v);
+	}
 }
